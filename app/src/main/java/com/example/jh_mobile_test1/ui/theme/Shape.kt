73aa46236f391d70f36ca6befa80598d370d6adb
Milosz Lewandowski <milosz.hg@gmail.com>
2022-05-14 18:28:36 +0200

    Initial commit

---
+++ app/src/main/java/com/example/jh_mobile_test1/ui/theme/Shape.kt
@@ -0,0 +1,11 @@
+package com.example.jh_mobile_test1.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Shapes
+import androidx.compose.ui.unit.dp
+
+val Shapes = Shapes(
+    small = RoundedCornerShape(4.dp),
+    medium = RoundedCornerShape(4.dp),
+    large = RoundedCornerShape(0.dp)
+)
---
+++ app/src/main/java/com/example/jh_mobile_test1/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.jh_mobile_test1.utils
+
+class Constants {
+
+    companion object{
+        const val BASE_IP = "http://146.59.35.71/"
+    }
+}
