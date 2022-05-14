35d2c104e189567c8ae4f3c563f00a82c1ae3877
Milosz Lewandowski <milosz.hg@gmail.com>
2022-05-14 20:22:52 +0200

    Api update

--- app/src/main/java/com/example/jh_mobile_test1/api/RetrofitInstance.kt
+++ app/src/main/java/com/example/jh_mobile_test1/api/RetrofitInstance.kt
@@ -1,19 +1,24 @@
 package com.example.jh_mobile_test1.api
 
-import com.example.jh_mobile_test1.utils.Constants.Companion.BASE_IP
+import com.example.jh_mobile_test1.utils.Constants.Companion.REMOTE_IP
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
 
 object RetrofitInstance {
 
     private val retrofit by lazy {
         Retrofit.Builder()
-            .baseUrl(BASE_IP)
+            .baseUrl(REMOTE_IP)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
 
     val therapistApi: TherapistApi by lazy {
         retrofit.create(TherapistApi::class.java)
+    }
+
+    val patientApi: PatientApi by lazy {
+        retrofit.create(PatientApi::class.java)
     }
 }
