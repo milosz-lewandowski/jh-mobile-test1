2364c672e76aec5d4e78159b7dcf00e67973f2bd
Milosz Lewandowski <milosz.hg@gmail.com>
2022-05-15 23:45:55 +0200

    Patient Repository

--- app/src/main/java/com/example/jh_mobile_test1/repository/PatientRepository.kt
+++ app/src/main/java/com/example/jh_mobile_test1/repository/PatientRepository.kt
@@ -3,7 +3,7 @@ package com.example.jh_mobile_test1.repository
 import com.example.jh_mobile_test1.api.RetrofitInstance
 import com.example.jh_mobile_test1.model.PatientList
 
-interface PatientRepository {
+class PatientRepository {
 
     suspend fun getPatientList(): PatientList {
         return RetrofitInstance.patientApi.getPatients()
